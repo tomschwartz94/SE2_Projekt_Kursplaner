40240bc1f49174eee1ebbb6e9e7dc8153631eddf
michelle <michelleanastasya.valentina@haw-hamburg.de>
2023-05-03 13:42:02 +0200

    added @repository annotations

--- src/main/java/com/se2/kursplaner/model/ModulRepository.java
+++ src/main/java/com/se2/kursplaner/model/ModulRepository.java
@@ -1,7 +1,9 @@
 package com.se2.kursplaner.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ModulRepository extends JpaRepository<Modul, Long> {
     
 }
--- src/main/java/com/se2/kursplaner/model/TerminRepository.java
+++ src/main/java/com/se2/kursplaner/model/TerminRepository.java
@@ -1,7 +1,9 @@
 package com.se2.kursplaner.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TerminRepository extends JpaRepository<Termin, Long> {
     
 }
