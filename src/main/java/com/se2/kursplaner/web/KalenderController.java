dca1229cca8677f524d22fb419617ee494ad1424
acx827 <simon.heinz@haw-hamburg.de>
2023-05-28 12:42:13 +0200

    init

--- src/main/java/com/se2/kursplaner/web/KalenderController.java
+++ src/main/java/com/se2/kursplaner/web/KalenderController.java
@@ -1,5 +1,28 @@
 package com.se2.kursplaner.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/kalender")
 public class KalenderController {
+
+
+    @GetMapping()
+    public List<Lecturer> getLecturers() {
+        return lecturerRepository.findAll(); // als json
+    }
+
+    @GetMapping(value = "/{id:[\\d]+}")
+    public Lecturer getLecturer(@PathVariable("id") Long lecturerId) throws LecturerNotFoundException {
+        return lecturerRepository
+                .findById(lecturerId)
+                .orElseThrow( () -> new LecturerNotFoundException(lecturerId));
+    }
+
     
 }
