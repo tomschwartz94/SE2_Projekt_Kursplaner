63e747f5f8757dafdcf537bffab0ebde91f5267f
acx827 <simon.heinz@haw-hamburg.de>
2023-06-02 18:33:22 +0200

    init controller

--- src/main/java/com/se2/kursplaner/web/ModulController.java
+++ src/main/java/com/se2/kursplaner/web/ModulController.java
@@ -1,5 +1,26 @@
-package com.se2.kursplaner.web;
+package com.se2.kursplaner.controller;
 
+import com.se2.kursplaner.model.Modul;
+import com.se2.kursplaner.model.ModulRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/modul")
 public class ModulController {
-    
+
+    @Autowired
+    private ModulRepository modulRepository;
+
+    @GetMapping
+    public List<Modul> getAllModul() {
+        return modulRepository.findAll();
+    }
+
+    @PostMapping
+    public Modul createModul(@RequestBody Modul modul) {
+        return modulRepository.save(modul);
+    }
 }
