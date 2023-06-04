7e295abba05b59208c5b75e662e2d9cb6d9d4420
acx827 <simon.heinz@haw-hamburg.de>
2023-06-04 13:55:41 +0200

    StudiengangController update

--- src/main/java/com/se2/kursplaner/web/StudiengangController.java
+++ src/main/java/com/se2/kursplaner/web/StudiengangController.java
@@ -1,22 +1,33 @@
 package com.se2.kursplaner.web;
 
-
 import com.se2.kursplaner.model.Studiengang;
+import com.se2.kursplaner.services.StudiengangService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-import org.springframework.web.bind.annotation.*;
-
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/studiengang")
 public class StudiengangController {
 
-    // Endpoint to get all Studiengänge
-    @GetMapping("/studiengang")
-    public List<Studiengang> getAllStudiengange() {
-        // <TO-DO>: Add your implementation to return all Studiengänge
-        return null;
+    @Autowired
+    StudiengangService studiengangService;
+
+
+    @GetMapping("/")
+    public ResponseEntity<List<Studiengang>> getAllStudiengange() {
+        List<Studiengang> studiengange = studiengangService.getAllStudiengange();
+
+        if (studiengange.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(studiengange, HttpStatus.OK);
     }
-
-
 }
