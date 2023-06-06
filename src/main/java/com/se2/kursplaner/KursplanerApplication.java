e0b16f2d77792254fb32c4cf6394b9161d596943
infwda636 <alexander.polter@haw-hamburg.de>
2023-06-06 15:07:20 +0200

    del not used imports

--- src/main/java/com/se2/kursplaner/KursplanerApplication.java
+++ src/main/java/com/se2/kursplaner/KursplanerApplication.java
@@ -1,11 +1,7 @@
 package com.se2.kursplaner;
 
-import com.se2.kursplaner.model.Studiengang;
-import com.se2.kursplaner.model.StudiengangRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
