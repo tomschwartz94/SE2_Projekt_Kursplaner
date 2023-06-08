6284f42abbe9494ebf475d2090c633b8fdd49986
acx827 <simon.heinz@haw-hamburg.de>
2023-06-08 14:47:42 +0200

    CollisonService refactoring

--- src/main/java/com/se2/kursplaner/services/CollisionService.java
+++ src/main/java/com/se2/kursplaner/services/CollisionService.java
@@ -22,19 +22,17 @@ public class CollisionService {
 
     public TerminVal checkTermine(List<Modul> module) {
 
-        Map<Modul, List<Termin>> allTermine = new HashMap<>();
+        List<List<Termin>> allValues = new ArrayList<>();
 
         TerminVal terminVal = new TerminVal();
 
         for(Modul m : module) {
-            allTermine.put(m, terminRepository.findByModul(m));
+            allValues.add(terminRepository.findByModul(m));
         }
 
-//        List<List<Termin>> allValues = (List<List<Termin>>) allTermine.values();
-        List<List<Termin>> allValues = new ArrayList<>(allTermine.values());
 
-        for(int i = 0; i < allTermine.size(); i++) {
-            for(int j = i + 1; j < allTermine.size(); j++) {
+        for(int i = 0; i < allValues.size(); i++) {
+            for(int j = i + 1; j < allValues.size(); j++) {
 
                 for (Termin t1:allValues.get(i)){
                     for (Termin t2:allValues.get(j)){
